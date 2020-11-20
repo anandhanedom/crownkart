86c0cb952688e98f57e837dc7d281adc5a728097
Anandhan <anandhanedom@gmail.com>
2020-11-20 22:35:31 +0530

    ss

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,9 +1,32 @@
 import { takeEvery } from 'redux-saga/effects';
 
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from './shop.actions';
+
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
   yield console.log('I am fired!');
+
+  const collectionRef = firestore.collection('collections');
+
+  //   collectionRef
+  //     .get()
+  //     .then((snapshot) => {
+  //       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+  //       dispatch(fetchCollectionsSuccess(collectionsMap));
+  //     })
+  //     .catch((err) => dispatch(fetchCollectionsFailure(err.message)));
+
+
+  
 }
 
 export function* fetchCollectionsStart() {
